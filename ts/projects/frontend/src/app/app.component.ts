a5d6124fa79d6dab40d334a6ef2f9267006e435e
Olivier Stuker <oli4stuker@hotmail.com>
2024-12-06 15:46:32 +0100

    Add ApiService prototype

--- ts/projects/frontend/src/app/app.component.ts
+++ ts/projects/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { ApiService } from './features/api/api.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { RouterOutlet } from '@angular/router'
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  // this is to prevent tree-shaking ApiService
+  constructor(private _apiService: ApiService) {}
+}
