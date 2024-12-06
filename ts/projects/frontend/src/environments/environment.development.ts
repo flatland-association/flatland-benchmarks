f2cac3b2fcf9f8ee0d5d6accfd8198886cce3e17
Olivier Stuker <oli4stuker@hotmail.com>
2024-12-06 15:43:43 +0100

    Add frontend environments

---
+++ ts/projects/frontend/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+// This is the environment file used during development (see angular.json)
+
+export const environment = {
+  // API base URL
+  apiBase: `${location.protocol}//${location.hostname}:8000`,
+}
---
+++ ts/projects/frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  // API base URL
+  apiBase: `${location.protocol}//${location.hostname}:8000`,
+}
