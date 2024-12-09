1acb94c8199b7e0db7d615132bb297f640e811ad
Olivier Stuker <oli4stuker@hotmail.com>
2024-12-09 11:45:38 +0100

    Add amqp prototype with amqplib and rabbitqm

--- ts/projects/backend/src/app/main.ts
+++ ts/projects/backend/src/app/main.ts
@@ -1,3 +1,4 @@
+import amqp from 'amqplib'
 import cors from 'cors'
 import type { Request } from 'express'
 import express from 'express'
@@ -55,24 +56,65 @@ function dbgRequestObject(req: Request) {
   }
 }
 
+// Returns JSON excerpt of request
 app.get('/mirror', (req, res) => {
   console.log(dbgRequestEndpoint(req))
   res.json(dbgRequestObject(req))
 })
 
+// Returns JSON excerpt of request
 app.get('/mirror/:id', (req, res) => {
   console.log(dbgRequestEndpoint(req))
   res.json(dbgRequestObject(req))
 })
 
+// Returns JSON excerpt of request
 app.post('/mirror', (req, res) => {
   console.log(dbgRequestEndpoint(req))
   res.json(dbgRequestObject(req))
 })
 
-app.listen(config.port, config.hostname, () => {
+// Returns last message in amqp queue
+app.get('/amqp', async (req, res) => {
+  console.log(dbgRequestEndpoint(req))
+  // connect to debug queue on rabbitmq server
+  const connection = await amqp.connect(`amqp://${config.amqp.host}:${config.amqp.port}`)
+  const channel = await connection.createChannel()
+  channel.assertQueue('debug', { durable: false })
+  // consume messages
+  let message: string | undefined = undefined
+  channel.consume('debug', (msg) => {
+    message = msg?.content.toString()
+  })
+  // await timeout (gives back control to main event loop for the consume callback)
+  await new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 100)
+  })
+  // close connection
+  connection.close()
+  // report last consumed message
+  res.json(`relayed from "debug": ${message ?? null}`)
+})
+
+// Posts a message to amqp queue
+app.post('/amqp', async (req, res) => {
+  console.log(dbgRequestEndpoint(req))
+  // connect to debug queue on rabbitmq server
+  const connection = await amqp.connect(`amqp://${config.amqp.host}:${config.amqp.port}`)
+  const channel = await connection.createChannel()
+  channel.assertQueue('debug', { durable: false })
+  // send message
+  channel.sendToQueue('debug', Buffer.from(JSON.stringify(req.body)))
+  // report what was sent
+  res.json(`relayed to "debug": ${JSON.stringify(req.body)}`)
+})
+
+// start express server
+app.listen(config.api.port, config.api.hostname, () => {
   console.log(
-    `${ansiEscapes.colors.fg.green}server is now live on ${config.hostname}:${config.port}`,
+    `${ansiEscapes.colors.fg.green}server is now live on ${config.api.hostname}:${config.api.port}`,
     ansiEscapes.reset,
   )
 })
