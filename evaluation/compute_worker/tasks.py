d063dcf61042a40fe369c5e580c74fb88549b303
chenkins <chenkins44@gmail.com>
2024-12-11 23:46:49 +0100

    Pass submission image name in mq message as well.

--- evaluation/compute_worker/tasks.py
+++ evaluation/compute_worker/tasks.py
@@ -13,37 +13,39 @@ HOST_DIRECTORY = os.environ.get("HOST_DIRECTORY", "/tmp/codabench/")
 
 # N.B. name to be used by send_task
 @app.task(name="doit")
-def the_task(docker_image: str):
+def the_task(docker_image: str, submission_image: str):
   loop = asyncio.new_event_loop()
   # TODO use compute_worker.py instead, which has most of the async spawning magic.
   future1 = loop.create_future()
   future2 = loop.create_future()
   gathered_tasks = asyncio.gather(
-    run_async_and_catch_output(future1, exec_args=["docker", "run",
-                                        "--rm",
-                                        "-e", "redis_ip=redis",
-                                                   # TODO workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....), establish convention for custom compute_workers...
-                                        "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/debug-environments/",
-                                        # TODO hacky: inject network name instead
-                                        "--network", "evaluation_default",
-                                        docker_image,
-                                                   # TODO document as entry point
-                                                   "bash", "run.sh"
-                                        ]),
-    run_async_and_catch_output(future2, exec_args=["docker", "run",
-                                        "--rm",
-                                        "-e", "redis_ip=redis",
-                                                   # TODO should data be mounted or...?
-                                        "-e", "AICROWD_TESTS_FOLDER=/tmp/debug-environments/",
-                                        # TODO workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....)
-                                        "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/debug-environments/",
-                                        # TODO hacky: inject network name instead
-                                        "--network", "evaluation_default",
-                                        # TODO hacky: extract image creation to submission submodule, extract from message?
-                                        "evaluation-submission:latest",
-                                                   # TODO document as entry point
-                                        "bash", "run.sh"
-                                        ]),
+    run_async_and_catch_output(future1, exec_args=[
+      "docker", "run",
+      "--rm",
+      "-e", "redis_ip=redis",
+      # TODO workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....), establish convention for custom compute_workers...
+      "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/debug-environments/",
+      # TODO hacky: inject network name instead
+      "--network", "evaluation_default",
+      docker_image,
+      # TODO document as entry point
+      "bash", "run.sh"
+    ]),
+    run_async_and_catch_output(future2, exec_args=[
+      "docker", "run",
+      "--rm",
+      "-e", "redis_ip=redis",
+      # TODO should data be mounted or...?
+      "-e", "AICROWD_TESTS_FOLDER=/tmp/debug-environments/",
+      # TODO workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....)
+      "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/debug-environments/",
+      # TODO hacky: inject network name instead
+      "--network", "evaluation_default",
+      # TODO hacky: extract image creation to submission submodule, extract from message?
+      submission_image,
+      # TODO document as entry point
+      "bash", "run.sh"
+    ]),
     loop=loop
   )
   loop.run_until_complete(gathered_tasks)
--- evaluation/submitter/submitter.py
+++ evaluation/submitter/submitter.py
@@ -13,7 +13,9 @@ if __name__ == '__main__':
     'doit',
     # TODO build docker image
     # TODO pass submission image as well - currently hard-coded to this same value
-    kwargs={"docker_image": "evaluation-evaluator:latest"},
+    kwargs={"docker_image": "evaluation-evaluator:latest",
+            "submission_image": "evaluation-submission:latest"
+            },
   ).get(timeout=10)
   assert rc == 0, (rc, stdout, stderr, rc2, stdout2, stderr2)
   print(f"\\ End simulate submission from portal: rc={rc}, stdout={stdout}, stderr={stderr}, rc2={rc2}, stdout2={stdout2}, stderr2={stderr2}")
