79ee18e6d369cecf625e5c6f82cb10bf8d518aa2
Olivier Stuker <oli4stuker@hotmail.com>
2025-01-10 07:59:30 +0100

    Add dummy content to home view

--- ts/frontend/src/app/views/home/home.view.ts
+++ ts/frontend/src/app/views/home/home.view.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core'
 import { ContentComponent, SectionComponent } from '@flatland-association/flatland-ui'
+import { FaIconComponent } from '@fortawesome/angular-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'view-home',
-  imports: [ContentComponent, SectionComponent],
+  imports: [ContentComponent, SectionComponent, FaIconComponent],
   templateUrl: './home.view.html',
   styleUrl: './home.view.scss',
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
-export class HomeView {}
+export class HomeView {
+  faPlus = faPlus
+}
