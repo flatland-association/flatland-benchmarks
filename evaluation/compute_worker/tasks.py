1952b1bedf4582be974533fb094b9c76f4583d83
chenkins <chenkins44@gmail.com>
2025-01-13 09:17:03 +0100

    Make docker network in compute worker configurable through env var.

--- evaluation/compute_worker/tasks.py
+++ evaluation/compute_worker/tasks.py
@@ -10,14 +10,17 @@ app = Celery(
 
 HOST_DIRECTORY = os.environ.get("HOST_DIRECTORY", "/tmp/codabench/")
 
+# TODO should we set up an ad-hoc network? What about communication to rabbit?
+BENCHMARKING_NETWORK = os.environ.get("BENCHMARKING_NETWORK", None)
 
 # docker run --rm -e redis_ip=redis -v /Users/che/workspaces/benchmarking/evaluation/evaluator/debug-environments/:/tmp/debug-environments/ --network evaluation_default evaluation-evaluator:latest bash run.sh
 # docker run --rm -e redis_ip=redis -e AICROWD_TESTS_FOLDER=/tmp/debug-environments/ -v /Users/che/workspaces/benchmarking/evaluation/evaluator/debug-environments/:/tmp/debug-environments/ --network evaluation_default evaluation-submission:latest bash run.sh
 # N.B. name to be used by send_task
 @app.task(name="doit")
 def the_task(docker_image: str, submission_image: str):
+  assert BENCHMARKING_NETWORK is not None
   loop = asyncio.new_event_loop()
-  # TODO use compute_worker.py instead, which has most of the async spawning magic.
+  # TODO use compute_worker.py instead, which has most of the async spawning magic?
   future1 = loop.create_future()
   future2 = loop.create_future()
   gathered_tasks = asyncio.gather(
@@ -28,7 +31,7 @@ def the_task(docker_image: str, submission_image: str):
       # TODO workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....), establish convention for custom compute_workers...
       "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/",
       # TODO hacky: inject network name instead
-      "--network", "evaluation_default",
+      "--network", BENCHMARKING_NETWORK,
       docker_image,
     ]),
     run_async_and_catch_output(future2, exec_args=[
@@ -39,9 +42,8 @@ def the_task(docker_image: str, submission_image: str):
       "-e", "AICROWD_TESTS_FOLDER=/tmp/debug-environments/",
       # TODO workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....)
       "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/debug-environments/",
-      # TODO allow only
-      "--network", "evaluation_default",
-      # TODO hacky: extract image creation to submission submodule, extract from message?
+      # TODO ensure no traffic going to the outside world.
+      "--network", BENCHMARKING_NETWORK,
       submission_image,
     ]),
     loop=loop
