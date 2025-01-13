3cf81f5fe9b43d723a3f42c00d39414f0ba1a852
chenkins <chenkins44@gmail.com>
2025-01-13 10:10:21 +0100

    Build evaluator and submission-template in GitHub ci and push to ghcr.io.

--- evaluation/submitter/submitter.py
+++ evaluation/submitter/submitter.py
@@ -3,19 +3,17 @@ import os
 from celery import Celery
 
 app = Celery(
-  broker=os.environ.get('BROKER_URL'),
-  backend='redis://localhost:6379',
+    broker=os.environ.get('BROKER_URL'),
+    backend='redis://localhost:6379',
 )
 
 if __name__ == '__main__':
-  print("/ Start simulate submission from portal.....")
-  ((rc, stdout, stderr), (rc2, stdout2, stderr2)) = app.send_task(
-    'flatland3-evaluation',
-    # TODO build docker image
-    # TODO pass submission image as well - currently hard-coded to this same value
-    kwargs={"docker_image": "evaluation-evaluator:latest",
-            "submission_image": "evaluation-submission:latest"
-            },
-  ).get(timeout=10)
-  assert rc == 0, (rc, stdout, stderr, rc2, stdout2, stderr2)
-  print(f"\\ End simulate submission from portal: rc={rc}, stdout={stdout}, stderr={stderr}, rc2={rc2}, stdout2={stdout2}, stderr2={stderr2}")
+    print("/ Start simulate submission from portal.....")
+    ((rc, stdout, stderr), (rc2, stdout2, stderr2)) = app.send_task(
+        'flatland3-evaluation',
+        kwargs={"docker_image": "ghcr.io/flatland-association/fab-flatland-evaluator:latest",
+                "submission_image": "ghcr.io/flatland-association/fab-flatland-submission-template:latest"
+                },
+    ).get(timeout=10)
+    assert rc == 0, (rc, stdout, stderr, rc2, stdout2, stderr2)
+    print(f"\\ End simulate submission from portal: rc={rc}, stdout={stdout}, stderr={stderr}, rc2={rc2}, stdout2={stdout2}, stderr2={stderr2}")
