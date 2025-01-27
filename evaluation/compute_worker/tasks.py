432b341a05752d37dc5d6c5826231b75d45af6e1
chenkins <chenkins44@gmail.com>
2025-01-27 15:22:42 +0100

    Spawn k8s job instead docker in compute worker.

--- evaluation/compute_worker/tasks.py
+++ evaluation/compute_worker/tasks.py
@@ -1,10 +1,12 @@
 # based on https://github.com/codalab/codabench/blob/develop/compute_worker/compute_worker.py
-import asyncio
 import logging
 import os
 import time
+import uuid
 
+import yaml
 from celery import Celery
+from kubernetes import client, config
 
 logger = logging.getLogger()
 
@@ -13,64 +15,81 @@ app = Celery(
   backend=os.environ.get('REDIS_IP'),
 )
 
-HOST_DIRECTORY = os.environ.get("HOST_DIRECTORY", "/tmp/codabench/")
-
-# TODO https://github.com/flatland-association/flatland-benchmarks/issues/27 should we set up an ad-hoc network? What about communication to rabbit?
-BENCHMARKING_NETWORK = os.environ.get("BENCHMARKING_NETWORK", None)
+KUBERNETES_NAMESPACE = os.environ.get("KUBERNETES_NAMESPACE", "nge-int")
 
 
 # TODO https://github.com/flatland-association/flatland-benchmarks/issues/27 start own redis for evaluator <-> submission communication? Split in flatland-repo?
 # N.B. name to be used by send_task
 @app.task(name="flatland3-evaluation", bind=True)
-def the_task(self, docker_image: str, submission_image: str):
+def the_task(self, docker_image: str, submission_image: str, config):
   task_id = self.request.id
+  config.load_incluster_config()
+  run_evaluation(task_id=task_id, docker_image=docker_image, submission_image=submission_image)
+
+
+def run_evaluation(task_id: str, docker_image: str, submission_image: str):
   start_time = time.time()
   logger.info(f"/ start task with task_id={task_id} with docker_image={docker_image} and submission_image={submission_image}")
-  assert BENCHMARKING_NETWORK is not None
-  loop = asyncio.new_event_loop()
-  evaluator_future = loop.create_future()
-  submission_future = loop.create_future()
-  gathered_tasks = asyncio.gather(
-    run_async_and_catch_output(evaluator_future, exec_args=[
-      "docker", "run",
-      "--rm",
-      "-e", "redis_ip=redis",
-      # TODO https://github.com/flatland-association/flatland-benchmarks/issues/27 workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....), establish convention for custom compute_workers...
-      "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/",
-      "--network", BENCHMARKING_NETWORK,
-      docker_image,
-    ]),
-    run_async_and_catch_output(submission_future, exec_args=[
-      "docker", "run",
-      "--rm",
-      "-e", "redis_ip=redis",
-      # TODO https://github.com/flatland-association/flatland-benchmarks/issues/27 https://github.com/flatland-association/flatland-benchmarks/issues/27 should data be mounted or...?
-      "-e", "AICROWD_TESTS_FOLDER=/tmp/debug-environments/",
-      # TODO https://github.com/flatland-association/flatland-benchmarks/issues/27 workaround as volumes come from host - will depend on where submissions come from (zip-minio, git,....)
-      "-v", f"{HOST_DIRECTORY}/evaluator/debug-environments/:/tmp/debug-environments/",
-      # TODO https://github.com/flatland-association/flatland-benchmarks/issues/27 ensure no traffic going to the outside world.
-      "--network", BENCHMARKING_NETWORK,
-      submission_image,
-    ]),
-    loop=loop
-  )
-  loop.run_until_complete(gathered_tasks)
+
+  # https://github.com/kubernetes-client/python/
+  # https://github.com/kubernetes-client/python/blob/master/examples/in_cluster_config.py
+
+  batch_api = client.BatchV1Api()
+  core_api = client.CoreV1Api()
+
+  evaluator_definition = yaml.safe_load(open("evaluator_job.yaml"))
+  evaluator_definition["metadata"]["name"] = f"{evaluator_definition['metadata']['name']}-{task_id}"
+  evaluator_definition["metadata"]["labels"]["task_id"] = task_id
+  evaluator_definition["spec"]["template"]["spec"]["containers"][0]["image"] = docker_image
+  evaluator = client.V1Job(metadata=evaluator_definition["metadata"], spec=evaluator_definition["spec"])
+  batch_api.create_namespaced_job(KUBERNETES_NAMESPACE, evaluator)
+
+  submission_definition = yaml.safe_load(open("submission_job.yaml"))
+  submission_definition["metadata"]["name"] = f"{submission_definition['metadata']['name']}-{task_id}"
+  submission_definition["metadata"]["labels"]["task_id"] = task_id
+  submission_definition["spec"]["template"]["spec"]["containers"][0]["image"] = submission_image
+  submission = client.V1Job(metadata=submission_definition["metadata"], spec=submission_definition["spec"])
+  batch_api.create_namespaced_job(KUBERNETES_NAMESPACE, submission)
+
+  done = False
+  status = []
+  logs = []
+  while not done:
+    print(".")
+    jobs = batch_api.list_namespaced_job(namespace=KUBERNETES_NAMESPACE, label_selector=f"task_id={task_id}")
+    assert len(jobs.items) == 2
+    done = True
+    status = []
+
+    for job in jobs.items:
+      done = done and job.status.conditions is not None
+      if job.status.conditions is not None:
+        status.append(job.status.conditions[0].type)
+
+    if done:
+      for job in jobs.items:
+        pods = core_api.list_namespaced_pod(namespace=KUBERNETES_NAMESPACE, label_selector=f"job-name={job.metadata.name}")
+        assert len(pods.items) == 1
+        pod = pods.items[0]
+        log = core_api.read_namespaced_pod_log(pod.metadata.name, namespace=KUBERNETES_NAMESPACE)
+        logs.append(log)
+
+  all_completed = all([s == "Complete" for s in status])
+  print(f"done {status}, all_completed={all_completed}")
   duration = time.time() - start_time
   logger.info(f"\\ end task with task_id={task_id} with docker_image={docker_image} and submission_image={submission_image}. Took {duration} seconds.")
-  return (evaluator_future.result(), submission_future.result())
+
+  # TODO dict instead?
+  ret = list(zip(status, logs))
+  print(ret)
+  return ret
 
 
-# based on https://github.com/codalab/codabench/blob/develop/compute_worker/compute_worker.py:_run_container_engine_cmd
-# no live WebSocket communication
-async def run_async_and_catch_output(future, exec_args):
-  logger.info(f"Run async {exec_args}")
-  proc = await  asyncio.create_subprocess_exec(
-    *exec_args,
-    stdout=asyncio.subprocess.PIPE,
-    stderr=asyncio.subprocess.PIPE
+if __name__ == '__main__':
+  TASK_ID = str(uuid.uuid4())
+  config.load_kube_config()
+  run_evaluation(
+    task_id=TASK_ID,
+    submission_image="swr.eu-ch2.sc.otc.t-systems.com/flatland-association/fab-flatland-submission-template:latest",
+    docker_image="swr.eu-ch2.sc.otc.t-systems.com/flatland-association/fab-flatland-evaluator:latest",
   )
-  stdout, stderr = await proc.communicate()
-  future.set_result((proc.returncode, stdout, stderr))
-  logger.info(f"task rc=%s", proc.returncode)
-  logger.debug(f"task stdout=%s", stdout)
-  logger.debug(f"task stderr=%s", stderr)
