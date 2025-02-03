3952d4647ccd24136f3b763e9b7b14f623c294a9
Olivier Stuker <oli4stuker@hotmail.com>
2025-02-03 15:13:30 +0100

    Add auth guard Change auth service to use implicit login flow

--- ts/frontend/src/app/app.routes.ts
+++ ts/frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router'
 import { ImpressumView, NotFoundView, PrivacyView } from '@flatland-association/flatland-ui'
+import { AuthGuard } from './guards/auth.guard'
 import { BenchmarksDetailView } from './views/benchmarks-detail/benchmarks-detail.view'
 import { HomeView } from './views/home/home.view'
 import { ParticipateView } from './views/participate/participate.view'
@@ -7,7 +8,7 @@ import { ParticipateView } from './views/participate/participate.view'
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeView },
-  { path: 'participate', component: ParticipateView },
+  { path: 'participate', component: ParticipateView, canActivate: [AuthGuard] },
   {
     path: 'association',
     redirectTo: () => {
@@ -15,7 +16,7 @@ export const routes: Routes = [
       return 'home'
     },
   },
-  { path: 'benchmarks/:id', component: BenchmarksDetailView },
+  { path: 'benchmarks/:id', component: BenchmarksDetailView, canActivate: [AuthGuard] },
   { path: 'impressum', component: ImpressumView },
   { path: 'privacy', component: PrivacyView },
   { path: '**', component: NotFoundView },
--- ts/frontend/src/app/features/auth/auth.service.ts
+++ ts/frontend/src/app/features/auth/auth.service.ts
@@ -1,4 +1,3 @@
-import { Location } from '@angular/common'
 import { Injectable } from '@angular/core'
 import { Router } from '@angular/router'
 import { OAuthService } from 'angular-oauth2-oidc'
@@ -13,6 +12,53 @@ export class AuthService {
   // This only works for forceful logins.
   readonly initialized?: Promise<unknown>
 
+  constructor(
+    public oauthService: OAuthService,
+    private router: Router,
+  ) {
+    // expose service for debugging purposes
+    //@ts-expect-error any
+    window['authService'] = this
+    // silently fail if no config was provided
+    if (!environment.authConfig) return
+    this.oauthService.configure(environment.authConfig)
+    this.oauthService.setupAutomaticSilentRefresh()
+    // Try logging user in, don't start login flow if it doesn't work.
+    // To start the login flow, call `logIn()`.
+    this.oauthService.loadDiscoveryDocumentAndTryLogin()
+    // As soon as a token is received, redirect to the passed state.
+    this.oauthService.events.pipe(first((e) => e.type === 'token_received')).subscribe(() => {
+      const state = decodeURIComponent(this.oauthService.state || '')
+      if (state && state !== '/') {
+        this.router.navigate([state])
+      }
+    })
+  }
+
+  /**
+   * Returns whether the user is logged in.
+   */
+  isLoggedIn() {
+    // assume user is logged in if in possession of valid token
+    return this.oauthService.hasValidAccessToken()
+  }
+
+  /**
+   * Starts the login flow. This function will redirect the user to the login
+   * form.
+   * @param state State passed around during login, used as redirect url on success.
+   */
+  logIn(state: string) {
+    this.oauthService.initLoginFlow(state)
+  }
+
+  /**
+   * Logs user out and redirects them to home.
+   */
+  logOut() {
+    return this.oauthService.logOut(false)
+  }
+
   get claims(): { email: string; name: string; roles: string[] } {
     return this.oauthService.getIdentityClaims() as {
       email: string
@@ -23,43 +69,6 @@ export class AuthService {
 
   get scopes(): string[] {
     return this.oauthService.getGrantedScopes() as string[]
-  }
-
-  constructor(
-    private oauthService: OAuthService,
-    private router: Router,
-    location: Location,
-  ) {
-    if (!environment.authConfig) return
-    this.oauthService.configure(environment.authConfig)
-    this.oauthService.setupAutomaticSilentRefresh()
-    // If the user should not be forcefully logged in (e.g. if you have pages, which can be
-    // accessed anonymously), change loadDiscoveryDocumentAndLogin to
-    // loadDiscoveryDocumentAndTryLogin and have a login functionality in the
-    // template of the component injecting the AuthService which calls the login() method.
-    this.initialized = this.oauthService
-      .loadDiscoveryDocumentAndLogin({ state: location.path() })
-      // If the user is not logged in, he will be forwarded to the identity provider
-      // and this promise will not resolve. After being redirected from the identity
-      // provider, the login promise will return true.
-      .then((v) =>
-        v
-          ? true
-          : new Promise(() => {
-              // * adf
-            }),
-      )
-    // Redirect the user to the url configured with state above or in a separate login call.
-    this.oauthService.events.pipe(first((e) => e.type === 'token_received')).subscribe(() => {
-      const state = decodeURIComponent(this.oauthService.state || '')
-      if (state && state !== '/') {
-        this.router.navigate([state])
-      }
-    })
-  }
-
-  logOut() {
-    return this.oauthService.logOut(false)
   }
 
   async hasRole(role: string) {
