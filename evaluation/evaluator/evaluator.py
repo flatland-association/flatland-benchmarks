05d71ce57ab50e6cad6dd5e2d5aca8b4420916f1
Christian Eichenberger <chenkins44@gmail.com>
2025-02-04 08:52:13 +0100

    Use task_id in results path

--- evaluation/evaluator/evaluator.py
+++ evaluation/evaluator/evaluator.py
@@ -1,10 +1,13 @@
+import os
+
 from flatland.evaluators.service import FlatlandRemoteEvaluationService
 
 if __name__ == '__main__':
   print("/ start grader", flush=True)
+  submission_id = os.getenv("AICROWD_SUBMISSION_ID", "T12345")
   grader = FlatlandRemoteEvaluationService(
     visualize=False,
-    result_output_path="/tmp/results/results.csv",
+    result_output_path=f"/tmp/results/results-{submission_id}.csv",
     verbose=True,
   )
   grader.run()
