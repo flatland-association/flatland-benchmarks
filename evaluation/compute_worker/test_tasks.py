8864b73f760959eafb8d4b05979731ac0b1700c7
chenkins <chenkins44@gmail.com>
2025-02-05 20:09:49 +0100

    Remove k8s retriever, get results from S3 instead.

--- evaluation/compute_worker/test_tasks.py
+++ evaluation/compute_worker/test_tasks.py
@@ -1,9 +1,6 @@
-import builtins
-import os
-import tarfile
-from pathlib import Path
+from base64 import b64encode
+from os import urandom
 
-import kubernetes
 import mockito
 from kubernetes.client import V1JobList, V1Job, V1JobStatus, V1JobCondition, V1ObjectMeta, BatchV1Api, CoreV1Api, V1PodList, V1Pod, V1PodStatus, V1PodCondition, \
   V1ContainerStatus
@@ -11,13 +8,13 @@ from mockito import mock
 from mockito import verify
 from mockito import when
 
-import tasks
 from tasks import run_evaluation
 
 
 def test_tasks_successful():
   core_api: CoreV1Api = mock()
   batch_api: BatchV1Api = mock()
+  s3 = mock()
 
   job_eva = V1Job(status=V1JobStatus(conditions=[V1JobCondition(type="Complete", status="blup")]), metadata=V1ObjectMeta(name=f"f3-evaluator-1234"))
   job_subi = V1Job(status=V1JobStatus(conditions=[V1JobCondition(type="Complete", status="blup")]), metadata=V1ObjectMeta(name=f"f3-submission-1234"))
@@ -45,28 +42,13 @@ def test_tasks_successful():
   when(core_api).connect_get_namespaced_pod_exec().thenReturn("")
 
   m = mock()
-  when(m).is_open().thenReturn(True, False)
-  when(m).peek_stdout().thenReturn(True, False)
-  when(m).read_stdout().thenReturn(os.urandom(4), os.urandom(4))
-  when(m).__enter__().thenReturn(None)
-  when(kubernetes.stream).stream(mockito.any(), mockito.any(), mockito.any(), command=['/bin/sh', '-c', 'cd /; tar czf - data'], binary=True, stderr=True,
-                                 stdin=True, stdout=True, tty=False, _preload_content=False).thenReturn(m)
-  tar: tarfile.TarFile = mock(tarfile.TarFile)
-  when(tar).__enter__().thenReturn(tar)
-  when(tar).__exit__(mockito.any(), mockito.any(), mockito.any())
-  when(tar).getmembers().thenReturn([])
-  when(tar).extractall(path=mockito.any(), members=mockito.any()).thenReturn([])
-  when(tarfile).open(fileobj=mockito.any(), mode=mockito.any()).thenReturn(tar)
-  f = mock()
-  when(f).read().thenReturn("lakjshdfljk")
-  builtins_open = builtins.open
-  when(builtins).open('/tmp/results-1234/data/results-1234.csv').thenReturn(f)
-  when(builtins).open('/tmp/results-1234/data/results-1234.json').thenReturn(f)
-  when(builtins).open(Path(tasks.__file__).parent / "evaluator_job.yaml").thenReturn(builtins_open(Path(tasks.__file__).parent / "evaluator_job.yaml"))
-  when(builtins).open(Path(tasks.__file__).parent / "submission_job.yaml").thenReturn(builtins_open(Path(tasks.__file__).parent / "submission_job.yaml"))
-  when(builtins).open(Path(tasks.__file__).parent / "retriever_pod.yaml").thenReturn(builtins_open(Path(tasks.__file__).parent / "retriever_pod.yaml"))
+  results_csv_bytes = b64encode(urandom(4))
+  results_json_bytes = b64encode(urandom(4))
+  when(m).read().thenReturn(results_csv_bytes, results_json_bytes)
+  when(s3).get_object(Bucket=mockito.any(), Key=mockito.any()).thenReturn({"Body": m})
 
-  ret = run_evaluation(task_id="1234", docker_image="fancy", submission_image="pancy", batch_api=batch_api, core_api=core_api)
+  ret = run_evaluation(task_id="1234", docker_image="fancy", submission_image="pancy", batch_api=batch_api, core_api=core_api, s3=s3,
+                       s3_upload_path_template="results/{}")
 
   verify(batch_api, times=1).list_namespaced_job(...)
   verify(core_api, times=1).list_namespaced_pod(namespace="fab-int", label_selector=f"job-name=f3-evaluator-1234")
@@ -93,4 +75,7 @@ def test_tasks_successful():
 
   assert ret["f3-evaluator"]["job"] == job_eva.to_dict()
   assert ret["f3-submission"]["job"] == job_subi.to_dict()
+
+  assert ret["f3-evaluator"]["results.csv"] == results_csv_bytes.decode("utf-8")
+  assert ret["f3-evaluator"]["results.json"] == results_json_bytes.decode("utf-8")
   mockito.unstub()
