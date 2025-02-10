44967e6fe5cf2ee2d3a392da692c836d11ca4d74
Olivier Stuker <oli4stuker@hotmail.com>
2025-02-10 16:43:24 +0100

    Add starter kit link to submission Add benchmark long description (hard-coded)

--- ts/frontend/src/app/views/benchmarks-detail/benchmarks-detail.view.ts
+++ ts/frontend/src/app/views/benchmarks-detail/benchmarks-detail.view.ts
@@ -3,14 +3,22 @@ import { Component, OnInit } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { ActivatedRoute, RouterModule } from '@angular/router'
 import { Benchmark, SubmissionPreview } from '@common/interfaces.mjs'
-import { ContentComponent } from '@flatland-association/flatland-ui'
+import { ContentComponent, SectionComponent } from '@flatland-association/flatland-ui'
 import { BreadcrumbsComponent } from '../../components/breadcrumbs/breadcrumbs.component'
 import { LeaderboardComponent } from '../../components/leaderboard/leaderboard.component'
 import { ApiService } from '../../features/api/api.service'
 
 @Component({
   selector: 'view-benchmarks-detail',
-  imports: [CommonModule, FormsModule, RouterModule, ContentComponent, BreadcrumbsComponent, LeaderboardComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ContentComponent,
+    SectionComponent,
+    BreadcrumbsComponent,
+    LeaderboardComponent,
+  ],
   templateUrl: './benchmarks-detail.view.html',
   styleUrl: './benchmarks-detail.view.scss',
 })
