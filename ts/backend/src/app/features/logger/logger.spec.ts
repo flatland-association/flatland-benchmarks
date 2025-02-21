7bb9ce7e548ddc6e346d1ffb19a35c82a06f721c
Olivier Stuker <oli4stuker@hotmail.com>
2025-02-21 12:01:50 +0100

    Change logger test to parameterized testing

--- ts/backend/src/app/features/logger/logger.spec.ts
+++ ts/backend/src/app/features/logger/logger.spec.ts
@@ -1,51 +1,106 @@
 import ansiStyles from 'ansi-styles'
-import { beforeAll, describe, expect, it, MockInstance, vi } from 'vitest'
+import { beforeAll, beforeEach, describe, expect, test, vi } from 'vitest'
 import { Logger } from './logger.mjs'
 
 describe('Logger', () => {
-  let logger: Logger
   let stdoutOutput = ''
-  let stdoutSpy: MockInstance
 
   beforeAll(() => {
-    // set options that differ from defaults
-    // for defaults, check Logger class
-    Logger.setOptions({
-      '--log-level': 'FATAL',
-      '--log-colorful': 'true',
-      '--log-stack': 'true',
-      '--log-stringify': 'true',
-    })
-    logger = new Logger('logger unit test')
-    stdoutSpy = vi.spyOn(process.stdout, 'write').mockImplementation((val) => {
-      stdoutOutput = val as string
+    // suppress actual output, store in memory instead
+    vi.spyOn(process.stdout, 'write').mockImplementation((val) => {
+      stdoutOutput += val as string
       return true
     })
   })
 
-  it('applies --log-level', () => {
-    // --log-level=FATAL - only .fatal() should write to stdout
-    logger.trace('noshow')
-    logger.debug('noshow')
-    logger.info('noshow')
-    logger.warn('noshow')
-    logger.error('noshow')
-    logger.fatal('show')
-    expect(stdoutSpy).toHaveBeenCalledTimes(1)
+  beforeEach(() => {
+    stdoutOutput = ''
   })
 
-  it('applies --log-colorful', () => {
+  // for defaults, see Logger class
+  test.each([
+    // --log-level - check only messages of configured level were output
+    {
+      title: 'should apply default level',
+      options: {},
+      expects: {
+        match: /INFO/, // info must be output
+        noMatch: /DEBUG/, // debug must not be output
+      },
+    },
+    {
+      title: 'should apply configured --log-level',
+      options: { '--log-level': 'FATAL' },
+      expects: {
+        match: /FATAL/, // must be output
+        noMatch: /ERROR/, // must not be output
+      },
+    },
     // --log-colorful - check presence of ansi-styles
-    expect(stdoutOutput.includes(ansiStyles.color.blue.open)).toBeTruthy()
-  })
-
-  it('applies --log-stack', () => {
+    // (if colored, a "color blue" ansi escape sequence will be present)
+    {
+      title: 'should apply default coloring',
+      options: {},
+      expects: {
+        noMatch: RegExp(escape(`${ansiStyles.color.blue.open}`)),
+      },
+    },
+    {
+      title: 'should apply configured --log-colorful',
+      options: { '--log-colorful': 'true' },
+      expects: {
+        match: RegExp(escape(`${ansiStyles.color.blue.open}`)),
+      },
+    },
     // --log-stack - expect stack format '(<caller> <file>:<line>:<char>)'
-    expect(stdoutOutput.match(/\([^\s]+ .+:\d+:\d+\)/)).toBeTruthy()
-  })
-
-  it('applies --log-stringify', () => {
+    {
+      title: 'should apply default stack option',
+      options: {},
+      expects: {
+        noMatch: /\([^\s]+ .+:\d+:\d+\)/,
+      },
+    },
+    {
+      title: 'should apply configured --log-stack option',
+      options: { '--log-stack': 'true' },
+      expects: {
+        match: /\([^\s]+ .+:\d+:\d+\)/,
+      },
+    },
     // --log-stringify - expect message to be stringified json
-    expect(stdoutOutput.includes(JSON.stringify(['show']))).toBeTruthy()
+    {
+      title: 'should apply default stringify option',
+      options: {},
+      expects: {
+        noMatch: /\["info"/,
+      },
+    },
+    {
+      title: 'should apply configured --log-stringify option',
+      options: { '--log-stringify': 'true' },
+      expects: {
+        match: /\["info"/,
+      },
+    },
+  ])('$title', ({ options, expects }) => {
+    Logger.setOptions(options)
+    const logger = new Logger('logger-unit-test')
+    logger.trace('trace', [options])
+    logger.debug('debug', [1])
+    logger.info('info', [1])
+    logger.warn('warn', [1])
+    logger.error('error', [1])
+    logger.fatal('fatal', [1])
+    if (expects?.match) {
+      expect(stdoutOutput).toMatch(expects.match)
+    }
+    if (expects?.noMatch) {
+      expect(stdoutOutput).not.toMatch(expects.noMatch)
+    }
   })
 })
+
+// escapes the square bracket for use in RegExp
+function escape(string: string) {
+  return string.replace('[', '\\[')
+}
