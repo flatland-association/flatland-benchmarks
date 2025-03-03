cc1bf7d4e8cc997f6eb787035a93a6ac9e77c6ea
Olivier Stuker <oli4stuker@hotmail.com>
2025-03-03 16:05:44 +0100

    Remove TestController, instead, test Controller directly

--- ts/backend/src/app/features/controller/controller.spec.ts
+++ ts/backend/src/app/features/controller/controller.spec.ts
@@ -3,46 +3,13 @@ import express from 'express'
 import supertest from 'supertest'
 import TestAgent from 'supertest/lib/agent'
 import { beforeAll, describe, expect, test } from 'vitest'
-import { configuration } from '../config/config.mjs'
 import { defaults } from '../config/defaults.mjs'
 import { Controller } from './controller.mjs'
-
-// Custom test controller to test Controller's basic functionality.
-// All `attach<Verb>` methods are aliases for the private `attach`. Hence let
-// us assume that by testing one (i.e. `attachGet`), all of them are tested
-// sufficiently.
-class TestController extends Controller {
-  constructor(config: configuration) {
-    super(config)
-
-    // can't attach to undefined endpoints, but can trick TS into see them as defined
-    this.attachGet('/test-get' as '/mirror', (req, res) => {
-      this.respond(res, '<ok>', '<debug>')
-    })
-    this.attachGet('/test-request-error' as '/mirror', (req, res) => {
-      this.requestError(res, { text: 'request error' })
-    })
-    this.attachGet('/test-auth-error' as '/mirror', (req, res) => {
-      this.unauthorizedError(res, { text: 'auth error' })
-    })
-    this.attachGet('/test-server-error' as '/mirror', (req, res) => {
-      this.serverError(res, { text: 'server error' })
-    })
-    // test fallback error handler with exception
-    this.attachGet('/test-catch' as '/mirror', (_req, _res) => {
-      throw 'error'
-    })
-    // test fallback error handler with undefined handler response
-    this.attachGet('/test-undefined' as '/mirror', (_req, _res) => {
-      /* */
-    })
-  }
-}
 
 describe.sequential('Controller', () => {
   let app: Express
   let request: TestAgent
-  let testController: Controller
+  let controller: Controller
 
   beforeAll(() => {
     app = express()
@@ -50,13 +17,40 @@ describe.sequential('Controller', () => {
   })
 
   test('should be instantiated (with default configuration)', () => {
-    testController = new TestController(defaults)
-    expect(testController.router).toBeTruthy()
-    app.use(testController.router)
+    controller = new Controller(defaults)
+    expect(controller.router).toBeTruthy()
+    app.use(controller.router)
+  })
+
+  // All `attach<Verb>` methods are aliases for the private `attach`. Hence let
+  // us assume that by testing one (i.e. `attachGet`), all of them are tested
+  // sufficiently.
+  test('should be able to attach handlers', () => {
+    // can't attach to undefined endpoints, but can trick TS into see them as defined
+    controller.attachGet('/test-get' as '/mirror', (req, res) => {
+      controller.respond(res, '<ok>', '<debug>')
+    })
+    controller.attachGet('/test-request-error' as '/mirror', (req, res) => {
+      controller.requestError(res, { text: 'request error' })
+    })
+    controller.attachGet('/test-auth-error' as '/mirror', (req, res) => {
+      controller.unauthorizedError(res, { text: 'auth error' })
+    })
+    controller.attachGet('/test-server-error' as '/mirror', (req, res) => {
+      controller.serverError(res, { text: 'server error' })
+    })
+    // test fallback error handler with exception
+    controller.attachGet('/test-catch' as '/mirror', (_req, _res) => {
+      throw 'error'
+    })
+    // test fallback error handler with undefined handler response
+    controller.attachGet('/test-undefined' as '/mirror', (_req, _res) => {
+      /* */
+    })
   })
 
   test('should have handlers attached', () => {
-    const routes = getControllerRoutes(testController)
+    const routes = getControllerRoutes(controller)
     expect(routes).toContain('/test-get')
     expect(routes).toContain('/test-request-error')
     expect(routes).toContain('/test-auth-error')
