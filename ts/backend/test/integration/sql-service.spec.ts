db2f6dc85287ebbb39aab1549e679c504cf8ac12
Olivier Stuker <oli4stuker@hotmail.com>
2025-03-07 11:41:11 +0100

    Bootstrap integration test

---
+++ ts/backend/test/integration/sql-service.spec.ts
@@ -0,0 +1,54 @@
+import { SqlService } from '../../src/app/features/services/sql-service.mjs'
+import { Schema } from '../../src/app/features/setup/schema.mjs'
+import { testConfig } from './setup.mjs'
+
+const SETUP_TIMEOUT = 30 * 1000 // ms
+
+describe.sequential('SQL Service (with Postgres)', () => {
+  beforeAll(async () => {
+    SqlService.create(testConfig)
+  })
+
+  test(
+    'should be able to run Schema.setup()',
+    async () => {
+      await expect(Schema.setup()).resolves.toBeUndefined()
+    },
+    SETUP_TIMEOUT,
+  )
+
+  test('should execute query `SELECT NOW()`', async () => {
+    const sql = SqlService.getInstance()
+    const rows = await sql.query`
+      SELECT NOW()
+    `
+    expect(rows.at(0)?.now).toBeInstanceOf(Date)
+  })
+
+  test('should execute query on set up schema', async () => {
+    const sql = SqlService.getInstance()
+    const rows = await sql.query`
+      SELECT id FROM benchmarks
+    `
+    expect(rows).toEqual([{ id: 1 }])
+  })
+
+  test('should error on faulty query (syntax error)', async () => {
+    const sql = SqlService.getInstance()
+    const rows = await sql.query`
+      SELEC
+    `
+    expect(rows).toEqual([])
+    expect(sql.errors).toBeTruthy()
+  })
+
+  test('should error on faulty query (schema error)', async () => {
+    const sql = SqlService.getInstance()
+    // Should this test ever fail maybe give the developer who decided to add a column named "shit emoji" to the benchmark table a stern talking-to.
+    const rows = await sql.query`
+      SELECT 💩 FROM benchmarks
+    `
+    expect(rows).toEqual([])
+    expect(sql.errors).toBeTruthy()
+  })
+})
---
+++ ts/backend/vitest.config.mts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  // tsconfigPaths is required to resolve path aliases ("paths") from tsconfig
+  plugins: [tsconfigPaths()],
+  test: {
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          include: ['./src/**/*.spec.ts'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'integration',
+          globalSetup: './test/integration/setup.mts',
+          include: ['./test/integration/**/*.spec.ts'],
+          globals: true,
+        },
+      },
+    ],
+  },
+})
