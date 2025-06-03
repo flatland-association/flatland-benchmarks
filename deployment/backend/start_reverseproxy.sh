e5d31f80a23e17f3b77f308ee3ac80e6e02833d3
chenkins <chenkins44@gmail.com>
2025-06-03 14:09:55 +0200

    Add debug output.

--- deployment/backend/start_reverseproxy.sh
+++ deployment/backend/start_reverseproxy.sh
@@ -3,4 +3,5 @@ apk add nginx
 nginx
 flyway -url=jdbc:postgresql://postgres:5432/${POSTGRES_DB} -schemas=public -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} migrate -connectRetries=60 -baselineOnMigrate=true
 cd /usr/src/app/dist/backend/app
+ls -al
 node main.min.mjs
