2f4a44e0927232dbc54d92d9b8a70d3b0f8570dc
chenkins <chenkins44@gmail.com>
2025-06-06 00:24:42 +0200

    Split orchestrator.py

--- evaluation/ai4realnet_orchestrators/domain_orchestrators/railway/orchestrator.py
+++ evaluation/ai4realnet_orchestrators/domain_orchestrators/railway/orchestrator.py
@@ -9,7 +9,7 @@ from uuid import uuid4
 import pandas as pd
 from celery import Celery
 
-from fab_oauth_utils import backend_application_flow
+from .test_runner_evaluator_557d9a00 import run_and_evaluate_test_557d9a00
 
 logger = logging.getLogger(__name__)
 
@@ -37,9 +37,7 @@ def orchestrator(self, submission_data_url: str, tests: List[str] = None, **kwar
     benchmark_id = orchestrator.name
     for test_id in tests:
         if test_id == "557d9a00-7e6d-410b-9bca-a017ca7fe3aa":
-            filename = Path(f"{test_id}_{submission_id}.json")
-            test_runner(submission_id=submission_id, test_id=test_id, submission_data_url=submission_data_url, filename=filename)
-            test_evaluator(submission_id=submission_id, test_id=test_id, filename=filename)
+            run_and_evaluate_test_557d9a00(submission_id=submission_id, test_id=test_id, submission_data_url=submission_data_url)
         elif test_id == "[INSERT HERE: @TestId]":
             pass
         else:
@@ -49,51 +47,3 @@ def orchestrator(self, submission_data_url: str, tests: List[str] = None, **kwar
         "status": "SUCCESS",
         "message": "message"
     }
-
-
-def test_runner(submission_id: str, test_id: str, submission_data_url: str, filename: Path):
-    """
-    Run a test and write output to a file "@SubmissionId_@TestId.json".
-
-    Parameters
-    ----------
-    submission_id
-    test_id
-    submission_data_url
-    filename
-
-    """
-    # run your experiment here and write results to "@TestId.json"
-    # TODO docker run -v ./data:/tmp -v ./entrypoint.sh:/home/conda/run.sh ghcr.io/flatland-association/flatland-baselines:latest --data-dir /tmp --policy-pkg flatland_baselines.deadlock_avoidance_heuristic.policy.deadlock_avoidance_policy --policy-cls DeadLockAvoidancePolicy --obs-builder-pkg flatland_baselines.deadlock_avoidance_heuristic.observation.full_env_observation --obs-builder-cls FullEnvObservation --ep-id ABCD
-    pass
-
-
-def test_evaluator(submission_id: str, test_id: str, filename: Path):
-    """
-    Load test output from file "@SubmissionId_@TestId.json", evaluate and upload to hub.
-
-    Parameters
-    ----------
-    submission_id
-    test_id
-    filename
-
-    Returns
-    -------
-
-    """
-
-    client_id = os.environ.get("CLIENT_ID", 'fab-client-credentials')
-    client_secret = os.environ.get("CLIENT_SECRET")
-    token_url = os.environ.get("TOKEN_URL",
-                               "https://keycloak.flatland.cloud/realms/netzgrafikeditor/protocol/openid-connect/token")  # TODO change to flatland realm
-    token = backend_application_flow(client_id, client_secret, token_url)
-    print(token)
-
-    # run your experiment here and write results to "@TestId.json"
-    if False:
-        df = pd.read_csv("./data/event_logs/TrainMovementEvents.trains_arrived.tsv", sep="\t")
-        assert len(df) == 1
-        print(df.iloc[0])
-        success_rate = df.iloc[0]["success_rate"]
-        print(success_rate)
