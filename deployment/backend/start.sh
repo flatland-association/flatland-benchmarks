2329411280d320eae331f18779949bf0348c1375
chenkins <chenkins44@gmail.com>
2025-06-05 16:55:24 +0200

    Make postgres host and port configurable in backend deployment.

--- deployment/backend/start.sh
+++ deployment/backend/start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-flyway -url=jdbc:postgresql://postgres:5432/${POSTGRES_DB} -schemas=public -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} migrate -connectRetries=60 -baselineOnMigrate=true
+flyway -url=jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -schemas=public -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} migrate -connectRetries=60 -baselineOnMigrate=true
 cd /usr/src/app/dist/backend/app
 node main.min.mjs
