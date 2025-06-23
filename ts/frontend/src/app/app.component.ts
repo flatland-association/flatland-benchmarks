6a49e9d0faa058e35700691e3872ae9207972268
Olivier Stuker <oli4stuker@hotmail.com>
2025-06-23 12:49:50 +0200

    Add customization service Add customization for FAB and AI4REALNET

--- ts/frontend/src/app/app.component.ts
+++ ts/frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject, OnInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { FooterNavLink, HeaderNavLink, LayoutComponent } from '@flatland-association/flatland-ui'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { OAuthModule } from 'angular-oauth2-oidc'
 import { ApiService } from './features/api/api.service'
 import { AuthService } from './features/auth/auth.service'
+import { CustomizationService } from './features/customization/customization.service'
 
 @Component({
   selector: 'app-root',
@@ -12,20 +13,25 @@ import { AuthService } from './features/auth/auth.service'
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private _apiService = inject(ApiService)
   private authService = inject(AuthService)
+  private customizationService = inject(CustomizationService)
 
-  headerNavItems: HeaderNavLink[] = [
-    {
-      path: '/home',
-      label: 'Home',
-      lead: 'Flatland Benchmarks',
-    },
-    { path: '/hub', label: 'Hub', icon: faArrowUpRightFromSquare },
-  ]
+  headerNavItems: HeaderNavLink[] = []
   footerNavItems: FooterNavLink[] = [
     { path: '/impressum', label: 'Impressum' },
     { path: '/privacy', label: 'Privacy' },
   ]
+
+  async ngOnInit() {
+    this.headerNavItems = [
+      {
+        path: '/home',
+        label: 'Home',
+        lead: (await this.customizationService.getCustomization()).content.title,
+      },
+      { path: '/hub', label: 'Hub', icon: faArrowUpRightFromSquare },
+    ]
+  }
 }
