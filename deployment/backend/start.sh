9282174cf4adafde9e0952db92500a16819a4d8a
chenkins <chenkins44@gmail.com>
2025-06-30 16:46:45 +0200

    ops: add public.ai4realnet and public.fab into docker image.

--- deployment/backend/start.sh
+++ deployment/backend/start.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 set -euxo pipefail
 
+if [[ -n "$CUSTOMIZATION" ]]; then
+   RUN cp -R /usr/src/public.${CUSTOMIZATION}/* /usr/src/app/dist/backend/public/
+fi
+
+
 flyway -url=jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -schemas=public -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} migrate -connectRetries=60 -baselineOnMigrate=true
 cd /usr/src/app/dist/backend/app
 node main.min.mjs
