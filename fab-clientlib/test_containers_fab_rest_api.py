dc7631305ead257f6ff9e2c63d3c20b628805091
chenkins <chenkins44@gmail.com>
2025-07-02 17:01:51 +0200

    test: add test getting results for benchmark.

--- fab-clientlib/test_containers_fab_rest_api.py
+++ fab-clientlib/test_containers_fab_rest_api.py
@@ -186,6 +186,38 @@ def test_submission_roundtrip():
     assert scenario_results2.body[0].scorings["secondary"]["score"] == 0.8
 
 
+# GET /results/benchmark/{benchmark_id}
+@pytest.mark.usefixtures("test_containers_fixture")
+def test_results_benchmark():
+    benchmark_id = '20ccc7c1-034c-4880-8946-bffc3fed1359'
+    test_id = '557d9a00-7e6d-410b-9bca-a017ca7fe3aa'
+
+    results = [
+        ('1ae61e4f-201b-4e97-a399-5c33fb75c57e', '557d9a00-7e6d-410b-9bca-a017ca7fe3aa', 'db5eaa85-3304-4804-b76f-14d23adb5d4c', 'primary', 100),
+        ('1ae61e4f-201b-4e97-a399-5c33fb75c57e', '557d9a00-7e6d-410b-9bca-a017ca7fe3aa', 'db5eaa85-3304-4804-b76f-14d23adb5d4c', 'secondary', 1.0),
+        ('564ebb54-48f0-4837-8066-b10bb832af9d', '557d9a00-7e6d-410b-9bca-a017ca7fe3aa', 'db5eaa85-3304-4804-b76f-14d23adb5d4c', 'primary', 101),
+        ('564ebb54-48f0-4837-8066-b10bb832af9d', '557d9a00-7e6d-410b-9bca-a017ca7fe3aa', 'db5eaa85-3304-4804-b76f-14d23adb5d4c', 'secondary', 0.8)
+    ]
+
+    token = backend_application_flow(
+        client_id='fab-client-credentials',
+        client_secret='top-secret',
+        token_url='http://localhost:8081/realms/flatland/protocol/openid-connect/token',
+    )
+    print(token)
+    fab = DefaultApi(ApiClient(configuration=Configuration(host="http://localhost:8000", access_token=token["access_token"])))
+
+    benchmark_results = fab.results_benchmarks_benchmark_id_get(benchmark_id=benchmark_id)
+    assert len(benchmark_results.body) == 1
+    assert benchmark_results.body[0].benchmark_id == benchmark_id
+    assert len(benchmark_results.body[0].items) == 1
+    assert benchmark_results.body[0].items[0].submission_id == "db5eaa85-3304-4804-b76f-14d23adb5d4c"
+    assert len(benchmark_results.body[0].items[0].scorings) == 2
+    assert benchmark_results.body[0].items[0].scorings["primary"]["score"] == 200
+    assert benchmark_results.body[0].items[0].scorings["secondary"]["score"] == 1.8
+    assert len(benchmark_results.body[0].items[0].test_scorings) == 1
+
+
 # GET /submissions/
 @pytest.mark.usefixtures("test_containers_fixture")
 def test_submissions_get():
