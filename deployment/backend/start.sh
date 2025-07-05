22f8b5099ff681c474685b36e28f473cfad115ec
Christian Eichenberger <chenkins44@gmail.com>
2025-07-05 09:14:57 +0200

    ops: allow args in entrypoint for fab backend. (#329)
    * ops: allow args in entrypoint for fab backend.
    
    * chore: release 0.9.1
    
    Release-As: 0.9.1

--- deployment/backend/start.sh
+++ deployment/backend/start.sh
@@ -8,4 +8,4 @@ fi
 
 flyway -url=jdbc:postgresql://${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB} -schemas=public -user=${POSTGRES_USER} -password=${POSTGRES_PASSWORD} migrate -connectRetries=60 -baselineOnMigrate=true
 cd /usr/src/app/dist/backend/app
-node main.min.mjs
+node main.min.mjs $@
