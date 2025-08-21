04fb9003ca95081f03a703e87952018c5f32dacc
chenkins <chenkins44@gmail.com>
2025-08-21 08:24:26 +0200

    refactor: extract list of fields to generate.

--- evaluation/flatland3_benchmarks/gen_flatland3_benchmarks_sql.py
+++ evaluation/flatland3_benchmarks/gen_flatland3_benchmarks_sql.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 
 if __name__ == '__main__':
 
+  test_type = 'CLOSED'
+
   if False:
     NUM_TESTS = 15
     NUM_LEVELS_PER_TEST = 10
@@ -13,63 +15,73 @@ if __name__ == '__main__':
     benchmark_group_id = None
     benchmark_name = "Round 1"
   else:
-    NUM_TESTS = 2
-    NUM_LEVELS_PER_TEST = 3
+    NUM_TESTS = 1
+    NUM_LEVELS_PER_TEST = 1
     test_descriptions = ['lorem ipsum'] * NUM_TESTS
     benchmark_group_id = "0ca46887-897a-463f-bf83-c6cd6269a976"
     # https://ai4realnet.eu/use-cases/
     benchmark_name = "Playground Electricity Network"
     benchmark_name = "Playground Air Traffic Management"
-    benchmark_name = "Playground Railway"
+    benchmark_name = "Playground Railway (interactive)"
+    test_type = 'CLOSED'
+    test_type = 'INTERACTIVE'
 
-  normalized_reward_scenario_field = str(uuid.uuid4())
-  normalized_reward_test_field = str(uuid.uuid4())
-  normalized_reward_benchmark_field = str(uuid.uuid4())
-  percentage_complete_scenario_field = str(uuid.uuid4())
-  percentage_complete_test_field = str(uuid.uuid4())
-  percentage_complete_benchmark_field = str(uuid.uuid4())
-  field_definitions = f"""INSERT INTO field_definitions
-  (id, key, description, agg_func, agg_weights)
-  VALUES
-  ('{normalized_reward_scenario_field}', 'normalized_reward', 'Scenario score (raw values)', NULL, NULL),
-  ('{normalized_reward_test_field}', 'normalized_reward', 'Test score (NANSUM of scenario scores)', 'NANSUM', NULL),
-  ('{normalized_reward_benchmark_field}', 'normalized_reward', 'Benchmark score (NANSUM of test scores)', 'NANSUM', NULL),
-  ('{percentage_complete_scenario_field}', 'percentage_complete', 'Scenario score (raw values)', NULL, NULL),
-  ('{percentage_complete_test_field}', 'percentage_complete', 'Test score (NANMEAN of scenario scores)', 'NANMEAN', NULL),
-  ('{percentage_complete_benchmark_field}', 'percentage_complete', 'Benchmark score (NANMEAN of test scores)', 'NANMEAN', NULL);
-  """
+  fields = [["normalized_reward", "NANSUM"], ["percentage_complete", "NANMEAN"]]
+
+  field_definitions = ""
+  scenario_fields = []
+  test_fields = []
+  benchmark_fields = []
+  for key, agg_func in fields:
+    scenario_field = str(uuid.uuid4())
+    test_field = str(uuid.uuid4())
+    benchmark_field = str(uuid.uuid4())
+    scenario_fields.append(scenario_field)
+    test_fields.append(test_field)
+    benchmark_fields.append(benchmark_field)
+
+    field_definitions += f"""INSERT INTO field_definitions
+    (id, key, description, agg_func, agg_weights)
+    VALUES
+    ('{scenario_field}', '{key}', 'Scenario score (raw values)', NULL, NULL),
+    ('{test_field}', '{key}', 'Test score ({agg_func} of scenario scores)', '{agg_func}', NULL),
+    ('{benchmark_field}', '{key}', 'Benchmark score ({agg_func} of test scores)', '{agg_func}', NULL),
+
+"""
 
   scenario_definitions = f"""INSERT INTO scenario_definitions
   (id, name, description, field_ids)
   VALUES
-  """
+"""
   scenario_ids = defaultdict(lambda: [])
   for i in range(NUM_TESTS):
     for j in range(NUM_LEVELS_PER_TEST):
       scenario_id = str(uuid.uuid4())
       scenario_ids[i].append(scenario_id)
-      scenario_definitions += f"""('{scenario_id}', 'Test {i} Level {j}', 'Test {i} Level {j}', array['{normalized_reward_scenario_field}','{percentage_complete_scenario_field}']::uuid[]),
+      scenario_definitions += f"""  ('{scenario_id}', 'Test {i} Level {j}', 'Test {i} Level {j}', array['{scenario_field.join("\', \'")}']::uuid[]),
+
 """
 
   scenario_definitions = re.sub(",\n$", ";", scenario_definitions)
 
   test_definitions = """INSERT INTO test_definitions
   (id, name, description, field_ids, scenario_ids, loop)
-VALUES
+  VALUES
 """
   test_ids = []
   for i, label in enumerate(test_descriptions):
     test_id = str(uuid.uuid4())
     test_ids.append(test_id)
-    test_definitions += f"""('{test_id}', 'Test {i}', '{label}', array['{normalized_reward_test_field}','{percentage_complete_test_field}']::uuid[], array['{"', '".join(scenario_ids[i])}']::uuid[], 'CLOSED'),
+    test_definitions += f"""  ('{test_id}', 'Test {i}', '{label}', array['{"\', \'".join(test_fields)}']::uuid[], array['{"', '".join(scenario_ids[i])}']::uuid[], '{test_type}'),
+
 """
   test_definitions = re.sub(",\n$", ";", test_definitions)
 
   benchmark_id = str(uuid.uuid4())
   benchmark_definitions = f"""INSERT INTO benchmark_definitions
   (id, name, description, field_ids, test_ids)
-VALUES
-  ('{benchmark_id}', '{benchmark_name}', '{benchmark_name}', array['{normalized_reward_benchmark_field}','{percentage_complete_benchmark_field}']::uuid[], array['{"', '".join(test_ids)}']::uuid[]);"""
+  VALUES
+  ('{benchmark_id}', '{benchmark_name}', '{benchmark_name}', array['{"\', \'".join(benchmark_fields)}']::uuid[], array['{"', '".join(test_ids)}']::uuid[]);"""
 
   print(field_definitions)
   print(scenario_definitions)
@@ -79,7 +91,7 @@ VALUES
     benchmark_group_id = str(uuid.uuid4())
     benchmark_group_definitions = f""" INSERT INTO benchmark_groups
   (id, setup, name, description, benchmark_ids)
-VALUES
+  VALUES
   ('{benchmark_group_id}', 'COMPETITION', 'Flatland 3 Benchmarks', 'The Flatland 3 Benchmarks.', array['{benchmark_id}']::uuid[]);"""
 
     print(benchmark_group_definitions)
