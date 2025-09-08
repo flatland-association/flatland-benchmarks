801ca500a69538d0d2af9cc4cf67de03205041ee
Olivier Stuker <oli4stuker@hotmail.com>
2025-09-08 09:10:35 +0200

    Add remark concerning wrong field definition used in campaign-overview

--- ts/frontend/src/app/components/campaign-overview/campaign-overview.component.ts
+++ ts/frontend/src/app/components/campaign-overview/campaign-overview.component.ts
@@ -54,6 +54,9 @@ export class CampaignOverviewComponent implements OnInit, OnChanges {
               params: { benchmark_ids: item.benchmark_id },
             })
           )?.at(0)
+          // TOFIX: in campaign, the relevant field ids are stored in
+          // campaign_field_ids, which needs to be included
+          // see: https://github.com/flatland-association/flatland-benchmarks/issues/364
           const fields = await this.resourceService.loadOrdered('/definitions/fields/:field_ids', {
             params: { field_ids: benchmark?.field_ids ?? [] },
           })
