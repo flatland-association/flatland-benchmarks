d2c68670e7792a2599f239904ef3cf3a6b18005e
Olivier Stuker <oli4stuker@hotmail.com>
2025-09-15 17:02:47 +0200

    Update sql generator to generate V10 schema valid data

--- definitions/ai4realnet/gen_ai4realnet_benchmarks_sql.py
+++ definitions/ai4realnet/gen_ai4realnet_benchmarks_sql.py
@@ -13,13 +13,13 @@ def extract_ai4realnet_from_csv(csv):
   data = defaultdict(lambda: {})
 
   for _, row in df.iterrows():
-    benchmark_group = data[row["BENCHMARK_GROUP_ID"]]
-    benchmark_group["ID"] = row["BENCHMARK_GROUP_ID"]
-    benchmark_group["BENCHMARK_GROUP_NAME"] = row["BENCHMARK_GROUP_NAME"]
-    benchmark_group["BENCHMARK_GROUP_DESCRIPTION"] = row["BENCHMARK_GROUP_DESCRIPTION"]
+    suite = data[row["SUITE_ID"]]
+    suite["ID"] = row["SUITE_ID"]
+    suite["SUITE_NAME"] = row["SUITE_NAME"]
+    suite["SUITE_DESCRIPTION"] = row["SUITE_DESCRIPTION"]
 
-    benchmark_group["benchmarks"] = benchmark_group.get("benchmarks", defaultdict(lambda: {}))
-    benchmarks = benchmark_group["benchmarks"]
+    suite["benchmarks"] = suite.get("benchmarks", defaultdict(lambda: {}))
+    benchmarks = suite["benchmarks"]
     benchmark = benchmarks[row["BENCHMARK_ID"]]
     benchmark["ID"] = row["BENCHMARK_ID"]
     benchmark["BENCHMARK_NAME"] = row["BENCHMARK_NAME"]
@@ -71,8 +71,8 @@ def gen_domain_orchestrator(data, domain):
 {domain.lower().replace(' ', '_')}_orchestrator = Orchestrator(
     test_runners={{
 """
-  for benchmark_group_id, benchmark_group in data.items():
-    for benchmark_id, benchmark in benchmark_group["benchmarks"].items():
+  for suite_id, suite in data.items():
+    for benchmark_id, benchmark in suite["benchmarks"].items():
       for test_id, test in benchmark["tests"].items():
         scenario_ids = [f"'{scenario_id}'" for scenario_id in test["scenarios"].keys()]
         if test["QUEUE"] == domain:
--- definitions/gen_benchmarks_common.py
+++ definitions/gen_benchmarks_common.py
@@ -44,10 +44,10 @@ def gen_sql_benchmark(benchmark_id, benchmark_name, benchmark_fields, test_ids):
 """
 
 
-def gen_sql_benchmark_group(benchmark_group_id, benchmark_group_setup, benchmark_group_name, benchmark_group_description, benchmark_ids):
-  return f""" INSERT INTO benchmark_groups
+def gen_sql_suite(suite_id, suite_setup, suite_name, suite_description, benchmark_ids):
+  return f""" INSERT INTO suites
     (id, setup, name, description, benchmark_ids)
-    VALUES ('{benchmark_group_id}', '{benchmark_group_setup}', '{benchmark_group_name}', '{benchmark_group_description}', array['{"', '".join(benchmark_ids)}']::uuid[])
+    VALUES ('{suite_id}', '{suite_setup}', '{suite_name}', '{suite_description}', array['{"', '".join(benchmark_ids)}']::uuid[])
     ON CONFLICT(id) DO UPDATE SET setup=EXCLUDED.setup, name=EXCLUDED.name, description=EXCLUDED.description, benchmark_ids=EXCLUDED.benchmark_ids;
 
 """
@@ -59,11 +59,11 @@ def escape_sql_string(s):
 
 def gen_sqls(data) -> str:
   sql = ""
-  for benchmark_group_id, benchmark_group in data.items():
-    sql += gen_sql_benchmark_group(benchmark_group_id, 'CAMPAIGN', benchmark_group["BENCHMARK_GROUP_NAME"], benchmark_group["BENCHMARK_GROUP_DESCRIPTION"],
-                                   list(benchmark_group["benchmarks"].keys()))
+  for suite_id, suite in data.items():
+    sql += gen_sql_suite(suite_id, 'CAMPAIGN', suite["SUITE_NAME"], suite["SUITE_DESCRIPTION"],
+                                   list(suite["benchmarks"].keys()))
 
-    for benchmark_id, benchmark in benchmark_group["benchmarks"].items():
+    for benchmark_id, benchmark in suite["benchmarks"].items():
       sql += gen_sql_benchmark(benchmark_id, benchmark["BENCHMARK_NAME"], [benchmark["BENCHMARK_FIELD_ID"]], list(benchmark["tests"].keys()))
       sql += gen_sql_test_benchmark_field(benchmark["BENCHMARK_FIELD_ID"], benchmark["BENCHMARK_FIELD_NAME"], benchmark["BENCHMARK_AGG"])
 
