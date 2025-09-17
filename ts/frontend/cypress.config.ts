07aa3be272b14c65dcc161f42f82f72aeaad206c
Olivier Stuker <oli4stuker@hotmail.com>
2025-09-17 14:54:23 +0200

    Add cypress-keycloak-commands Complete e2e roundtrip

--- ts/frontend/cypress.config.ts
+++ ts/frontend/cypress.config.ts
@@ -4,4 +4,9 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
   },
+  env: {
+    auth_base_url: 'http://localhost:8081',
+    auth_realm: 'flatland',
+    auth_client_id: 'fab',
+  },
 })
--- ts/frontend/cypress/e2e/spec.cy.ts
+++ ts/frontend/cypress/e2e/spec.cy.ts
@@ -5,7 +5,13 @@ describe('Complete roundtrip', () => {
     cy.contains('Welcome to the validation campaign hub.')
   })
 
-  it('should navigate to the demo data campaign', () => {
+  it('should navigate to the demo data campaign, make, score and publish submission', () => {
+    // Using time of day in submission name to further identify it.
+    // This makes it possible to run e2e tests multiple times a day without
+    // having to wipe the dev DB.
+    const timeInfix = new Date().toISOString().substring(11, 19)
+    const submissionName = `E2E Submission (${timeInfix})`
+    cy.kcLogin('user')
     cy.visit('/')
     cy.get('app-benchmark-group-card > div').first().click()
     // the demo data campaign will have zero submissions for AI-human learning
@@ -20,6 +26,23 @@ describe('Complete roundtrip', () => {
     cy.contains('tr', 'KPI-AS-006').click()
     // start submission
     cy.contains('button', 'Submit').click()
-    // TODO: login
+    // enter details and submit
+    cy.get('input#submission-name').type(submissionName)
+    cy.contains('button', 'Submit').click()
+    // navigate to scenario
+    cy.contains('tr', 'KPI-AS-006-1').click()
+    // enter score and submit
+    cy.get('input[type=number]').type('500')
+    cy.contains('button', 'Submit').click()
+    // navigate back to submission via breadcrumb
+    cy.get('app-breadcrumbs').contains('a', submissionName).click()
+    // assert it's been successfully score and publish
+    cy.contains('This submission has been successfully scored.')
+    cy.contains('button', 'Publish').click()
+    // navigate back to KPI via breadcrumb
+    cy.get('app-breadcrumbs').contains('a', 'AI-human learning curves').click()
+    // assert it's been published by visiting the KPI and looking for it
+    cy.contains('tr', 'KPI-AS-006').click()
+    cy.contains('tr', submissionName)
   })
 })
