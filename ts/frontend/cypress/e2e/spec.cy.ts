2cc28a3c699cb966c74bbce316d2bf4dfa57fb3f
Olivier Stuker <oli4stuker@hotmail.com>
2025-09-19 13:07:51 +0200

    Identify elements in e2e test by data attributes

--- ts/frontend/cypress/e2e/spec.cy.ts
+++ ts/frontend/cypress/e2e/spec.cy.ts
@@ -15,31 +15,31 @@ describe('Complete roundtrip', () => {
     const submissionName = `${submission.name} (${timeSuffix})`
     cy.kcLogin('user')
     cy.visit('/')
-    // navigate to suite
-    cy.get('app-benchmark-group-card > div').first().click()
+    // navigate to suite (by id for test stability, because the name is shared)
+    cy.get(`[data-link*=${submission.suiteId}]`).click()
     // navigate to benchmark
-    cy.contains('tr', submission.benchmark).as('objectiveRow').click()
+    cy.contains('[data-link]', submission.benchmark).as('objectiveRow').click()
     // navigate to KPI
-    cy.contains('tr', submission.test).click()
+    cy.contains('[data-link]', submission.test).click()
     // start submission
-    cy.contains('button', 'Submit').click() // TODO: identify by data-* attribute
+    cy.get('[data-btn-new-submission]').click()
     // enter details and submit
-    cy.get('input#submission-name').type(submissionName)
-    cy.contains('button', 'Submit').click() // TODO: identify by data-* attribute
+    cy.get('[data-input-submission-name]').type(submissionName)
+    cy.get('[data-btn-submit]').click()
     // navigate to scenario
-    cy.contains('tr', submission.scenario).click()
+    cy.contains('[data-link]', submission.scenario).click()
     // enter score and submit
-    cy.get('input[type=number]').type(submission.score)
-    cy.contains('button', 'Submit').click() // TODO: identify by data-* attribute
+    cy.get('[data-input-cell-number]').type(submission.score)
+    cy.get('[data-btn-submit]').click()
     // navigate back to submission via breadcrumb
     cy.get('app-breadcrumbs').contains('a', submissionName).click()
     // assert it's been successfully score and publish
-    cy.contains('This submission has been successfully scored.')
-    cy.contains('button', 'Publish').click() // TODO: identify by data-* attribute
+    cy.get('[data-text-success]')
+    cy.get('[data-btn-publish]').click()
     // navigate back to KPI via breadcrumb
-    cy.get('app-breadcrumbs').contains('a', 'AI-human learning curves').click()
+    cy.get('app-breadcrumbs').contains('a', submission.benchmark).click()
     // assert it's been published by visiting the KPI and looking for it
-    cy.contains('tr', submission.test).click()
+    cy.contains('[data-link]', submission.test).click()
     cy.contains('tr', submissionName)
   })
 })
