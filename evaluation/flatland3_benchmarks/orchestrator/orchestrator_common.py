573de801a52c4806f7331bdeab4049bd352d49fb
chenkins <chenkins44@gmail.com>
2025-10-20 23:39:25 +0200

    test(flatland benchmarks): use offline evluation docker compose orchestrator for tests.
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- evaluation/flatland3_benchmarks/orchestrator/orchestrator_common.py
+++ evaluation/flatland3_benchmarks/orchestrator/orchestrator_common.py
@@ -1,17 +1,13 @@
 # based on https://github.com/codalab/codabench/blob/develop/orchestrator/orchestrator.py
-import json
 import logging
 import os
 import time
+import traceback
 from abc import abstractmethod
-from io import StringIO
 from typing import Dict
 from typing import List
 
-import boto3
 import celery.exceptions
-import numpy as np
-import pandas as pd
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
@@ -40,7 +36,6 @@ class FlatlandBenchmarksOrchestrator:
   def orchestrator(self,
                    submission_data_url: str,
                    tests: List[str] = None,
-                   test_runner_evaluator_image="ghcr.io/flatland-association/fab-flatland3-benchmarks-evaluator:latest",
                    aws_endpoint_url=None,
                    aws_access_key_id=None,
                    aws_secret_access_key=None,
@@ -67,33 +62,12 @@ class FlatlandBenchmarksOrchestrator:
         s3_upload_path_template = os.environ.get("S3_UPLOAD_PATH_TEMPLATE", None)
       if s3_upload_path_template_use_submission_id is None:
         s3_upload_path_template_use_submission_id = os.environ.get("S3_UPLOAD_PATH_TEMPLATE_USE_SUBMISSION_ID", None)
-      ret = self.run_flatland(test_runner_evaluator_image, submission_id, submission_data_url, tests, aws_endpoint_url, aws_access_key_id,
-                              aws_secret_access_key, s3_bucket, s3_upload_path_template, s3_upload_path_template_use_submission_id, **kwargs)
+      ret = self.run_flatland(submission_id, submission_data_url, tests, aws_endpoint_url, aws_access_key_id, aws_secret_access_key, s3_bucket,
+                              s3_upload_path_template, s3_upload_path_template_use_submission_id, **kwargs)
 
       duration = time.time() - start_time
       logger.info(
-        f"\\ end task with submission_id={submission_id} with docker_image={test_runner_evaluator_image} and submission_data_url={submission_data_url}. Took {duration:.2f} seconds.")
-
-      logger.info("Get results files from S3 under %s...", aws_endpoint_url)
-      if s3 is None:
-        s3 = boto3.client(
-          's3',
-          # https://docs.weka.io/additional-protocols/s3/s3-examples-using-boto3
-          endpoint_url=aws_endpoint_url,
-          aws_access_key_id=aws_access_key_id,
-          aws_secret_access_key=aws_secret_access_key
-        )
-      obj = s3.get_object(Bucket=s3_bucket, Key=s3_upload_path_template.format(submission_id) + ".csv")
-      ret["f3-evaluator"]["results.csv"] = obj['Body'].read().decode("utf-8")
-      obj = s3.get_object(Bucket=s3_bucket, Key=s3_upload_path_template.format(submission_id) + ".json")
-      ret["f3-evaluator"]["results.json"] = obj['Body'].read().decode("utf-8")
-
-      print("results.csv")
-      df_results = pd.read_csv(StringIO(ret["f3-evaluator"]["results.csv"]))
-      print(df_results)
-      print("results.json")
-      json_results = json.loads(ret["f3-evaluator"]["results.json"])
-      print(json_results)
+        f"\\ end task with submission_id={submission_id} with submission_data_url={submission_data_url}. Took {duration:.2f} seconds.")
 
       if fab is None:
         token = backend_application_flow(CLIENT_ID, CLIENT_SECRET, TOKEN_URL)
@@ -101,36 +75,34 @@ class FlatlandBenchmarksOrchestrator:
         print(token)
         fab = DefaultApi(ApiClient(configuration=Configuration(host=FAB_API_URL, access_token=token["access_token"])))
 
-      for _, row in df_results.iterrows():
-        print("uploading results for row:")
-        print(row)
-        if np.isnan(row["normalized_reward"]):
-          print("skipping")
-          continue
-        fab.results_submissions_submission_id_tests_test_ids_post(
-          submission_id=submission_id,
-          test_ids=[row["fab_test_id"]],
-          results_submissions_submission_id_tests_test_ids_post_request=ResultsSubmissionsSubmissionIdTestsTestIdsPostRequest(
-            data=[
-              ResultsSubmissionsSubmissionIdTestsTestIdsPostRequestDataInner(
-                scenario_id=row["fab_scenario_id"],
-                scores={
-                  "normalized_reward": row["normalized_reward"],
-                  "percentage_complete": row["percentage_complete"]
-                },
-              )
-            ]
-          ),
-        )
+      for test_id, scenarios in ret.items():
+        for scenario_id, result in scenarios.items():
+          print(f"uploading results for submission_id={submission_id},test_id={test_id}, scenario_id={scenario_id}: {result}")
+
+          fab.results_submissions_submission_id_tests_test_ids_post(
+            submission_id=submission_id,
+            test_ids=[test_id],
+            results_submissions_submission_id_tests_test_ids_post_request=ResultsSubmissionsSubmissionIdTestsTestIdsPostRequest(
+              data=[
+                ResultsSubmissionsSubmissionIdTestsTestIdsPostRequestDataInner(
+                  scenario_id=scenario_id,
+                  scores=result,
+                )
+              ]
+            ),
+          )
       return ret
 
     except celery.exceptions.SoftTimeLimitExceeded as e:
-      logger.info("Hit %s - getting logs from containers", e)
+      logger.error("Hit %s - getting logs from containers", e)
       raise e
+    except BaseException as e:
+      logger.error("Hit %s - get results from S3 and uploading to FAB", e)
+      raise Exception(f"Hit {e} - get results from S3 and uploading to FAB: {traceback.format_exception(e)}") from e
 
   @abstractmethod
-  def run_flatland(self, test_runner_evaluator_image, submission_id, submission_data_url, tests, aws_endpoint_url, aws_access_key_id, aws_secret_access_key,
-                   s3_bucket, s3_upload_path_template, s3_upload_path_template_use_submission_id, **kwargs):
+  def run_flatland(self, submission_id, submission_data_url, tests, aws_endpoint_url, aws_access_key_id, aws_secret_access_key, s3_bucket,
+                   s3_upload_path_template, s3_upload_path_template_use_submission_id, **kwargs):
     raise NotImplementedError()
 
 
