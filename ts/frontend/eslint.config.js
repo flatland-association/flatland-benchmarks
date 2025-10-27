9dd8a11cf4e155065a269ef6f473ea42f5f8fb79
Olivier Stuker <oli4stuker@hotmail.com>
2025-10-27 09:03:12 +0100

    Change coverage output path

--- ts/frontend/eslint.config.js
+++ ts/frontend/eslint.config.js
@@ -40,7 +40,7 @@ module.exports = tseslint.config(
     },
   },
   {
-    files: ["**/*.html"],
+    files: ["src/**/*.html"],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
