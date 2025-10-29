a076ee75ac4b56d49890d3b1e85c224e7e5af60d
chenkins <chenkins44@gmail.com>
2025-10-29 16:47:08 +0100

    !refactor: use offline trajectory generation for k8s orchestrator as well.
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- evaluation/flatland3_benchmarks/orchestrator/s3_utils.py
+++ evaluation/flatland3_benchmarks/orchestrator/s3_utils.py
@@ -11,8 +11,9 @@ S3_BUCKET = os.getenv("S3_BUCKET", "aicrowd-production")
 class s3_utils:
 
   @staticmethod
-  def upload_to_s3(localpath, relative_upload_key, s3_bucket=S3_BUCKET, ):
-    s3 = s3_utils.get_boto_client()
+  def upload_to_s3(localpath, relative_upload_key, s3_bucket=S3_BUCKET, s3=None):
+    if s3 is None:
+      s3 = s3_utils.get_boto_client()
     if not s3_bucket:
       raise Exception("S3_BUCKET not provided...")
 
@@ -50,3 +51,43 @@ class s3_utils:
       return False
     else:
       return True
+
+
+# https://stackoverflow.com/questions/31918960/boto3-to-download-all-files-from-a-s3-bucket
+def download_dir(prefix, local, bucket, client):
+  """
+  params:
+  - prefix: pattern to match in s3
+  - local: local path to folder in which to place files
+  - bucket: s3 bucket with target contents
+  - client: initialized s3 client object
+  """
+  keys = []
+  dirs = []
+  next_token = ''
+  base_kwargs = {
+    'Bucket': bucket,
+    'Prefix': prefix,
+  }
+  while next_token is not None:
+    kwargs = base_kwargs.copy()
+    if next_token != '':
+      kwargs.update({'ContinuationToken': next_token})
+    results = client.list_objects_v2(**kwargs)
+    contents = results.get('Contents')
+    for i in contents:
+      k = i.get('Key')
+      if k[-1] != '/':
+        keys.append(k)
+      else:
+        dirs.append(k)
+    next_token = results.get('NextContinuationToken')
+  for d in dirs:
+    dest_pathname = os.path.join(local, d)
+    if not os.path.exists(os.path.dirname(dest_pathname)):
+      os.makedirs(os.path.dirname(dest_pathname))
+  for k in keys:
+    dest_pathname = os.path.join(local, k)
+    if not os.path.exists(os.path.dirname(dest_pathname)):
+      os.makedirs(os.path.dirname(dest_pathname))
+    client.download_file(bucket, k, dest_pathname)
