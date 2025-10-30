c7ed11ebb32f5b8d03d32bd96679cf228b438690
Olivier Stuker <oli4stuker@hotmail.com>
2025-10-30 14:57:29 +0100

    Remove deprecated utils

--- ts/common/scoring-utils.ts
+++ ts/common/scoring-utils.ts
@@ -6,27 +6,3 @@ import { SubmissionScenarioScore, SubmissionScore, SubmissionTestScore } from '.
 export function isScored(itemScore?: SubmissionScore | SubmissionTestScore | SubmissionScenarioScore) {
   return typeof itemScore?.scorings[0].score === 'number'
 }
-
-/**
- * Returns whether all submission's scores are numerical.
- */
-export function isSubmissionCompletelyScored(submissionScore?: SubmissionScore) {
-  if (!submissionScore) return false
-  return submissionScore.scorings.every((scoring) => typeof scoring.score === 'number')
-}
-
-/**
- * Returns whether all tests's scores are numerical.
- */
-export function isTestCompletelyScored(testScore?: SubmissionTestScore) {
-  if (!testScore) return false
-  return testScore.scorings.every((scoring) => typeof scoring.score === 'number')
-}
-
-/**
- * Returns whether all scenario's scores are numerical.
- */
-export function isScenarioCompletelyScored(scenarioScore?: SubmissionScenarioScore) {
-  if (!scenarioScore) return false
-  return scenarioScore.scorings.every((scoring) => typeof scoring.score === 'number')
-}
