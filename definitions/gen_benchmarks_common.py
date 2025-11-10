4f49024444f6174f760f939dac3116a6afdbd2fa
chenkins <chenkins44@gmail.com>
2025-11-10 17:49:25 +0100

    build: move examples with latest schema defintions (v13).

--- definitions/gen_benchmarks_common.py
+++ definitions/gen_benchmarks_common.py
@@ -1,5 +1,5 @@
 def gen_sql_test_benchmark_field(test_or_benchmark_field, key, field_description, agg_func):
-  return f"""INSERT INTO field_definitions
+  return f"""INSERT INTO fields
         (id, key, description, agg_func, agg_weights)
         VALUES ('{test_or_benchmark_field}', '{key}', '{field_description}', '{agg_func}', NULL)
         ON CONFLICT(id) DO UPDATE SET key=EXCLUDED.key, description=EXCLUDED.description, agg_func=EXCLUDED.agg_func, agg_weights=EXCLUDED.agg_weights;
@@ -8,7 +8,7 @@ def gen_sql_test_benchmark_field(test_or_benchmark_field, key, field_description
 
 
 def gen_sql_scenario_field(key, scenario_field, scenario_field_description):
-  return f"""INSERT INTO field_definitions
+  return f"""INSERT INTO fields
         (id, key, description, agg_func, agg_weights)
         VALUES ('{scenario_field}', '{key}', '{scenario_field_description}', NULL, NULL)
         ON CONFLICT(id) DO UPDATE SET key=EXCLUDED.key, description=EXCLUDED.description, agg_func=EXCLUDED.agg_func, agg_weights=EXCLUDED.agg_weights;
@@ -17,7 +17,7 @@ def gen_sql_scenario_field(key, scenario_field, scenario_field_description):
 
 
 def gen_sql_scenario(scenario_id, scenario_name, scenario_description, scenario_fields):
-  return f"""INSERT INTO scenario_definitions
+  return f"""INSERT INTO scenarios
     (id, name, description, field_ids)
     VALUES ('{scenario_id}', '{escape_sql_string(scenario_name)[:1024]}', '{escape_sql_string(scenario_description)[:1024]}', array['{"\', \'".join(scenario_fields)}']::uuid[])
     ON CONFLICT(id) DO UPDATE SET name=EXCLUDED.name, description=EXCLUDED.description, field_ids=EXCLUDED.field_ids;
@@ -27,7 +27,7 @@ def gen_sql_scenario(scenario_id, scenario_name, scenario_description, scenario_
 
 def gen_sql_test(test_id, test_name, test_description, test_fields, scenario_ids_for_test, test_type, queue: str = None):
   queue_ = "NULL" if queue is None else f"'{queue.replace(' ', '')}'"
-  return f"""INSERT INTO test_definitions
+  return f"""INSERT INTO tests
     (id, name, description, field_ids, scenario_ids, loop, queue)
     VALUES ('{test_id}', '{test_name}', '{escape_sql_string(test_description)}', array['{"\', \'".join(test_fields)}']::uuid[], array['{"', '".join(scenario_ids_for_test)}']::uuid[], '{test_type}', {queue_})
     ON CONFLICT(id) DO UPDATE SET name=EXCLUDED.name, description=EXCLUDED.description, field_ids=EXCLUDED.field_ids, scenario_ids=EXCLUDED.scenario_ids, loop=EXCLUDED.loop, queue=EXCLUDED.queue;
@@ -36,7 +36,7 @@ def gen_sql_test(test_id, test_name, test_description, test_fields, scenario_ids
 
 
 def gen_sql_benchmark(benchmark_id, benchmark_name, benchmark_description, benchmark_fields, test_ids):
-  return f"""INSERT INTO benchmark_definitions
+  return f"""INSERT INTO benchmarks
     (id, name, description, field_ids, test_ids)
     VALUES ('{benchmark_id}', '{benchmark_name}', '{benchmark_description}', array['{"\', \'".join(benchmark_fields)}']::uuid[], array['{"', '".join(test_ids)}']::uuid[])
     ON CONFLICT(id) DO UPDATE SET name=EXCLUDED.name, description=EXCLUDED.description, field_ids=EXCLUDED.field_ids, test_ids=EXCLUDED.test_ids;
