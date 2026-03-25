7e0892595e2d321455d7a5bb0e50289b9ce422da
chenkins <chenkins44@gmail.com>
2026-03-25 17:09:21 +0100

    ops: add manual test allocated memory can be used effectively.

--- evaluation/flatland3_benchmarks/orchestrator/cleankeeping/k8s_sanity_checking.py
+++ evaluation/flatland3_benchmarks/orchestrator/cleankeeping/k8s_sanity_checking.py
@@ -63,26 +63,75 @@ def test_oom_fail_fast(submission_data_url="ghcr.io/flatland-association/flatlan
     s3_url_environments_zip=S3_URL_ENVIRONMENTS_ZIP,
     submissions_pvc=SUBMISSIONS_PVC,
     active_deadline_seconds=55,
+    k8s_resource_allocation='{"requests": {"memory": "1Gi", "cpu": "1"}, "limits": {"memory": "2Gi", "cpu": "2"}}',
   )
 
   orchestrator._make_command = lambda *args, **kwargs: ["bash", "-c"]
   orchestrator._make_args = lambda *args, **kwargs: ["""
-  pip install numpy
-  python -c 'import numpy as np; print(np.ones((10000000000,)))'
+      swapoff -a
+      # 2Gi
+      head -c $((2 * 1024**3)) /dev/zero | tail > /dev/null
       """]
 
   with pytest.raises(TaskExecutionError) as exc_info:
     start_time = time.time()
-    orchestrator._run_submission(
+    ret = orchestrator._run_submission(
       test_id,
       scenario_id,
       submission_data_url,
       pkl_path
     )
+    print(ret)
   end_time = time.time()
   elapsed_time = end_time - start_time
   print(exc_info.value.message)
   assert "OOM" in exc_info.value.message
+  # may fail the image needs to be pulled.
+  assert elapsed_time < 30
+
+
+def test_no_oom_respecting_memory_limit(submission_data_url="ghcr.io/flatland-association/flatland-baselines-random:latest",
+                                        test_id="fc8f5fb1-4525-4b4f-a022-d3d7800097dc",
+                                        scenario_id="289394a5-aa51-446c-9b62-c25101643790",
+                                        pkl_path="Test_00/Level_0.pkl",
+                                        ):
+  config.load_kube_config()
+  core_api = client.CoreV1Api()
+  batch_api = client.BatchV1Api()
+  submission_id = str(uuid.uuid4())
+
+  orchestrator = K8sFlatlandBenchmarksOrchestrator(
+    submission_id=submission_id,
+    batch_api=batch_api,
+    core_api=core_api,
+    kubernetes_namespace=KUBERNETES_NAMESPACE,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_endpoint_url=AWS_ENDPOINT_URL,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    s3_bucket=S3_BUCKET,
+    s3_url_environments_zip=S3_URL_ENVIRONMENTS_ZIP,
+    submissions_pvc=SUBMISSIONS_PVC,
+    active_deadline_seconds=55,
+    k8s_resource_allocation='{"requests": {"memory": "1Gi", "cpu": "1"}, "limits": {"memory": "2Gi", "cpu": "2"}}',
+  )
+
+  orchestrator._make_command = lambda *args, **kwargs: ["bash", "-c"]
+  orchestrator._make_args = lambda *args, **kwargs: ["""
+      swapoff -a
+      # 2Gi - 50 Mi
+      head -c $((2 * 1024**3 - 50 * 1024**2)) /dev/zero | tail > /dev/null
+      """]
+
+  start_time = time.time()
+  orchestrator._run_submission(
+    test_id,
+    scenario_id,
+    submission_data_url,
+    pkl_path
+  )
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+
   # may fail the image needs to be pulled.
   assert elapsed_time < 30
 
@@ -195,6 +244,58 @@ def test_time_max_running_time_respected_succeeds(submission_data_url="ghcr.io/f
                                                   ):
   """
   Verify allocated time can be used effectively.
+  """
+  config.load_kube_config()
+  core_api = client.CoreV1Api()
+  batch_api = client.BatchV1Api()
+  submission_id = str(uuid.uuid4())
+
+  orchestrator = K8sFlatlandBenchmarksOrchestrator(
+    submission_id=submission_id,
+    batch_api=batch_api,
+    core_api=core_api,
+    kubernetes_namespace=KUBERNETES_NAMESPACE,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_endpoint_url=AWS_ENDPOINT_URL,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    s3_bucket=S3_BUCKET,
+    s3_url_environments_zip=S3_URL_ENVIRONMENTS_ZIP,
+    submissions_pvc=SUBMISSIONS_PVC,
+    active_deadline_seconds=active_deadline_seconds,
+    running_time_limit=running_time_limit,
+  )
+  orchestrator._make_command = lambda *args, **kwargs: ["bash", "-c"]
+
+  orchestrator._make_args = lambda *args, **kwargs: [f""" sleep {running_time_limit - lower_delta} """]
+  start_time = time.time()
+  orchestrator._run_submission(
+    test_id,
+    scenario_id,
+    submission_data_url,
+    pkl_path
+  )
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+
+  # may fail the image needs to be pulled.
+  print(
+    f"assert {elapsed_time}s in [{running_time_limit - lower_delta:.2f},{running_time_limit - lower_delta:.2f}] {running_time_limit:.2f}+{upper_delta:.2f}-{lower_delta:.2f}")
+  assert running_time_limit + upper_delta > elapsed_time > running_time_limit - lower_delta
+
+
+def test_time_max_memory_respected_succeeds(submission_data_url="ghcr.io/flatland-association/flatland-baselines-random:latest",
+                                            test_id="fc8f5fb1-4525-4b4f-a022-d3d7800097dc",
+                                            scenario_id="289394a5-aa51-446c-9b62-c25101643790",
+                                            pkl_path="Test_00/Level_0.pkl",
+                                            active_deadline_seconds=55,
+                                            # allow for some overhead (starting the pod)
+                                            upper_delta=10,
+                                            # allow for some overhead (running)
+                                            lower_delta=3,
+                                            running_time_limit=25
+                                            ):
+  """
+  Verify allocated memory can be used effectively.
   """
   config.load_kube_config()
   core_api = client.CoreV1Api()
