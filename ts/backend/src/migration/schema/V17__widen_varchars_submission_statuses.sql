d2b3eb4ede917527b7b90694a5629ad250d3d95d
chenkins <chenkins44@gmail.com>
2026-04-21 14:14:51 +0200

    ops(railway-orchestrator): widen varchars in submissions.

--- ts/backend/src/migration/schema/V17__widen_varchars_submission_statuses.sql
+++ ts/backend/src/migration/schema/V17__widen_varchars_submission_statuses.sql
@@ -3,4 +3,4 @@
 --
 
 ALTER TABLE submission_statuses ALTER COLUMN message TYPE VARCHAR(1024);
-ALTER TABLE submission_statuses ALTER COLUMN tags TYPE VARCHAR(1024);
+ALTER TABLE submissions ALTER COLUMN tags TYPE VARCHAR(1024);
