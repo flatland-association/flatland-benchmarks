019cd14f07c7c1ce175ac96b484f0d43415c9edd
chenkins <chenkins44@gmail.com>
2026-06-03 14:13:30 +0200

    fix(backend): allow only `Admin`s to `POST` submission status not their own.

--- ts/backend/test/integration/submission.controller.spec.ts
+++ ts/backend/test/integration/submission.controller.spec.ts
@@ -385,7 +385,10 @@ describe.sequential('Submission controller', () => {
   })
 
   test('should allow admin to post status to any submission', async ({ skip }) => {
-    if (!submissionUuid) skip()
+    const submission = await controller.testPost('/submissions', { body: testSubmission }, testUserJwt)
+    assertApiResponse(submission)
+    submissionUuid = submission.body.body.id
+
     const res = await controller.testPost(
       `/submissions/:submission_ids/statuses`,
       {
