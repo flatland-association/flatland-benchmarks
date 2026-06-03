97b7de2d78c1df2edc5dd46b319c5bab319ac935
chenkins <chenkins44@gmail.com>
2026-06-03 16:13:54 +0200

    feat(frontend): in competition setup, display leaderboard directly if there is only one round

--- ts/frontend/src/app/views/suite/suite.view.ts
+++ ts/frontend/src/app/views/suite/suite.view.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { SuiteDefinitionRow } from '@common/interfaces'
+import { BenchmarkDefinitionRow, SuiteDefinitionRow } from '@common/interfaces'
 import { ContentComponent } from '@flatland-association/flatland-ui'
 import { Subscription } from 'rxjs'
+import { BenchmarkOverviewComponent } from '../../components/benchmark-overview/benchmark-overview.component'
 import { CampaignOverviewComponent } from '../../components/campaign-overview/campaign-overview.component'
 import { SiteHeadingComponent } from '../../components/site-heading/site-heading.component'
 import { SuiteOverviewComponent } from '../../components/suite-overview/suite-overview.component'
@@ -20,6 +21,7 @@ import { PublicResourcePipe } from '../../pipes/public-resource/public-resource.
     CampaignOverviewComponent,
     SuiteOverviewComponent,
     TabsComponent,
+    BenchmarkOverviewComponent,
   ],
   templateUrl: './suite.view.html',
   styleUrl: './suite.view.scss',
@@ -32,6 +34,7 @@ export class SuiteView implements OnInit, OnDestroy {
 
   suite?: SuiteDefinitionRow
   customization?: Customization
+  firstBenchmark?: BenchmarkDefinitionRow
 
   ngOnInit(): void {
     this.customizationService.getCustomization().then((customization) => {
@@ -42,6 +45,19 @@ export class SuiteView implements OnInit, OnDestroy {
         .load('/definitions/suites/:suite_ids', { params: { suite_ids: suite_id } })
         .then((suites) => {
           this.suite = suites?.at(0)
+          console.log(`Got suite ${this.suite}`)
+          const benchmark_id = this.suite?.benchmark_ids.at(0)
+          if (benchmark_id) {
+            console.log(`Fetching first benchmark ${benchmark_id}`)
+            this.resourceService
+              .load('/definitions/benchmarks/:benchmark_ids', {
+                params: { benchmark_ids: [benchmark_id] },
+              })
+              .then((benchmark) => {
+                this.firstBenchmark = benchmark?.at(0)
+                console.log(`Got first benchmark ${this.firstBenchmark}`)
+              })
+          }
         })
     })
   }
